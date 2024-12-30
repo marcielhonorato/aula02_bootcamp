c39e750cd94cb605144b619814c33e613000a5af
marciel_honorato <marciel.hsouza@hotmail.com>
2024-12-30 12:03:56 -0300

    exercicio03 adicionado

--- exercicios.py
+++ exercicios.py
@@ -13,18 +13,22 @@
 
 #Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 
-num000 = int(input("Digite um numero: "))
+# num000 = int(input("Digite um numero: "))
 
-resto = num000 % 5
+# resto = num000 % 5
 
-print(f"O resto da divisão é: {resto}")
+# print(f"O resto da divisão é: {resto}")
 
 #Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+num0001 = int(input("Digite o primeiro número: "))
+num0002 = int(input("Digite o segundo número: "))
+
+resultado = num0001 / num0002
+
+print (f"O resultado da multiplicação é {resultado}")
+
 #Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
-
-
-
-
 #Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 #Números de Ponto Flutuante (float)
 #Escreva um programa que receba dois números flutuantes e realize sua adição.
