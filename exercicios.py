1f20df09ebaedc66a0c3f45495e4bb143ab8d41b
marciel_honorato <marciel.hsouza@hotmail.com>
2024-12-30 11:36:31 -0300

    exercicio01 adicionado

--- exercicios.py
+++ exercicios.py
@@ -9,9 +9,12 @@ num02 = int(input("Digite o segundo número:"))
 
 soma = num01 + num02
 
-print("A soma dos numero digitados é? ")
+print(f"A soma dos numero digitados é {soma}")
 
 #Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+
+
 #Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 #Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 #Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
