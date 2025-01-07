532757c49a006ba31d5bcd6022ba51c77a78aef1
marciel_honorato <marciel.hsouza@hotmail.com>
2025-01-07 10:58:37 -0300

    exercicio15 adicionado

--- exercicios.py
+++ exercicios.py
@@ -117,15 +117,22 @@ import math
 
 #14 Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 
-data = input("Digite uma data no formato dd/mm/aaaa: ")
-split_data = data.split("/")
-dia = split_data[0]
-mes = split_data[1]
-ano = split_data[2]
+    # data = input("Digite uma data no formato dd/mm/aaaa: ")
+    # split_data = data.split("/")
+    # dia = split_data[0]
+    # mes = split_data[1]
+    # ano = split_data[2]
 
-print(f"Dia: {dia}, Mês: {mes}, Ano: {ano}")
+    # print(f"Dia: {dia}, Mês: {mes}, Ano: {ano}")
 
 #15 Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+palavra00 = input("Digite a primeira palavra: ")
+palavra01 = input("Digite a segunda palavra: ")
+
+resultado = palavra00 + ' ' + palavra01
+
+print(f"a concatenação das palavras digitadas é {resultado}")
 
 ### Booleanos (bool)
 
