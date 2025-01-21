dc346641c1309c946231e287f24e6f529cb8f4ee
marciel_honorato <marciel.hsouza@hotmail.com>
2025-01-21 16:51:38 -0300

    exercicio23 adicionado

--- exercicios.py
+++ exercicios.py
@@ -194,22 +194,37 @@ import math
 # Crie um programa que verifica se uma palavra ou frase é um palíndromo (lê-se igualmente de trás para frente, desconsiderando espaços e pontuações). 
 # Utilize try-except para garantir que a entrada seja uma string. Dica: Utilize a função isinstance() para verificar o tipo da entrada.
 
-entrada = input("Digite uma palavra ou frase: ")
-if isinstance(entrada, str):
-    formatado = entrada.replace(" ", "").lower()
-    if formatado == formatado[::-1]:
-        print("É um palíndromo.")
-    else:
-        print("Não é um palíndromo.")
-else:
-    print("Entrada inválida. Por favor, digite uma palavra ou frase.")
+    # entrada = input("Digite uma palavra ou frase: ")
+    # if isinstance(entrada, str):
+    #     formatado = entrada.replace(" ", "").lower()
+    #     if formatado == formatado[::-1]:
+    #         print("É um palíndromo.")
+    #     else:
+    #         print("Não é um palíndromo.")
+    # else:
+    #     print("Entrada inválida. Por favor, digite uma palavra ou frase.")
 
 # Exercício 23: Calculadora Simples
 # Desenvolva uma calculadora simples que aceite duas entradas numéricas e um operador (+, -, *, /) do usuário. Use try-except para lidar com divisões por zero e entradas não numéricas.
 # Utilize if-elif-else para realizar a operação matemática baseada no operador fornecido. Imprima o resultado ou uma mensagem de erro apropriada.
 
+try:
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    operador = input("Digite o operador (+, -, *, /): ")
+    if operador == '+':
+        resultado = num1 + num2
+    elif operador == '-':
+        resultado = num1 - num2
+    elif operador == '*':
+        resultado = num1 * num2
+    elif operador == '/' and num2 != 0:
+        resultado = num1 / num2
+    else:
+        print("Operador inválido ou divisão por zero.")
 
-
+except ValueError:
+    print("ERRO: Entrada Inválida. Certifique-se de inserir números.")
 
 # Exercício 24: Classificador de Números
 # Escreva um programa que solicite ao usuário para digitar um número. Utilize try-except para assegurar que a entrada seja numérica e utilize if-elif-else para classificar o número como 
